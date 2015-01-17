8b1533551d2f840910c2f948f0b3a69c1211b30f
Ho-Wei Kang <howeik@gmail.com>
2015-01-17 00:50:57 -0800

    added new event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,17 @@
 class EventsController < ApplicationController
   def index
     # call Carl's API for getting events and filter them...
+    @events = [
+      {
+        'title' => 'Flyers GBM'
+      },
+      {
+        'title' => 'Flyers Bowling Night!!!'
+      }
+    ]
+  end
+
+  def new
+
   end
 end
