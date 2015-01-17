167f36b186cae4aa339c61a52bc7c6360003caf6
Kevin Gu <kevingu@Charless-MacBook-Pro.local>
2015-01-17 01:16:10 -0800

    added gem rest-client gem-file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
+require 'rest_client'
 class EventsController < ApplicationController
   def index
     # call Carl's API for getting events and filter them...
+    RestClient.get('http://54.183.116.48/events')
   end
 end
