16ee3651d6bcc0887a35196a4fa8d7ceb90d937f
Kevin Gu <kevingu@Charless-MacBook-Pro.local>
2015-01-17 02:12:28 -0800

    added new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,9 @@ class EventsController < ApplicationController
   end
 
   def new
-    
+    RestClient.post('http://54.183.116.48/events',{:title=>params[:title], :startDate=>params[:startDate],
+                      :endDate=>params[:endDate], :description=>params[:description], :images=>params[:images],
+                      :organization=>params[:organization], :heroImage=>params[:heroImage], :location=>params[:location],
+                      :visibility=>params[:visibility], :status=>params[:status]})
   end
 end
